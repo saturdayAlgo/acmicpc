cce437a382197c703561ff37bc2bc0533d1325d9
Hrpfs Lee <hrpfsn@gmail.com>
2020-06-20 17:59:35 +0900

    6/20

---
+++ jh/p01926/p01926.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	int i, j, r, c;
+	scanf("%d %d", &i, &j);
+
+	for(r = 0; r < j; r++){
+
+		for(c = 0; c < i; c++){//column
+			
+
+		}	
+
+	}
+
+
+
+	return 0;
+}
---
+++ jh/p02309/p02309.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+
+	int sum = 0;
+
+
+	for(int n = 0; n < 9; n++){
+		cin >> m[n];
+		sum += m[n];
+	}
+
+	cout << '\n';
+
+	for(int i = 0; i < 9; i++){
+		
+	}
+	
+
+	return 0;
+
+}
