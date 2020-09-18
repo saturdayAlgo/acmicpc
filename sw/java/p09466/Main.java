7f62eb5cc324e61ca73592ab5bc337a37d3d9290
povia <tjddn020@naver.com>
2020-09-18 17:55:40 +0900

    [SW] temporal update

--- sw/java/p09466/Main.java
+++ sw/java/p09466/Main.java
@@ -2,8 +2,11 @@ package p09466;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,32 +18,67 @@ public class Main {
 		for(int i=0; i<N; i++) {
 			int K = Integer.parseInt(br.readLine());
 			int[] dataSet = new int[K+1];
-			int[] trueList = new int[K+1];
+			boolean[] trueList = new boolean[K+1];
+			Arrays.fill(trueList, false);
 			st = new StringTokenizer(br.readLine()); 
 			for(int j=1; j<=K; j++) {
 				int tgt = Integer.parseInt(st.nextToken());
-				if(tgt==j) {
-					trueList[j] = 1;
-				} else {
-					dataSet[j] = tgt;
+				if(tgt==j){
+					trueList[j] = true;
 				}
+				dataSet[j] = tgt;
 			}
 			
 			// -1일 경우 => visit은 했으나 팀을 찾지 못한 경우
 			// 0 => visit==false
-			int level = 2;
+			
 			for(int j=1; j<=K; j++) {
-				Queue<Integer> qu = new LinkedList<Integer>();
-				if(dataSet[j]>0 && trueList[j]==0) {
+				if (!trueList[j]) {
+					boolean[] visited = new boolean[K + 1];
+					Arrays.fill(visited, false);
+					Stack<Integer> qu = new Stack<Integer>();
 					qu.add(dataSet[j]);
-					trueList[j]=level;
-				}
-				while(!qu.isEmpty()) {
-					
+					visited[j] = true;
+					boolean isTeam = false;
+					ArrayList<Integer> teams = new ArrayList<>();
+					teams.add(j);
+					while (!qu.isEmpty()) {
+						
+						int tgt = qu.pop();
+						if (dataSet[tgt] == j) {
+							teams.add(tgt);
+							isTeam = true;
+							break;
+						}
+
+						int nextNum = dataSet[tgt];
+						if(!visited[nextNum]){
+							qu.add(nextNum);
+							teams.add(tgt);
+							visited[nextNum] = true;
+						}
+						
+					}
+					if (isTeam) {
+						for(int num : teams){
+							trueList[num] = true;
+						}
+					}
 				}
 				
-				level++;
 			}
+			
+			int result = 0;
+			for(int j=1; j<=K; j++){
+				if(!trueList[j]){
+					result++;
+				}
+			}
+			System.out.println(result);
 		}
+	}
+	
+	public static void dfs(){
+		
 	}
 }
