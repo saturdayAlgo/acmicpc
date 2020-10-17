bc8bf4031a19c8429c32a5b900d021965a7d6f16
povia <tjddn020@naver.com>
2020-10-17 15:03:38 +0900

    [SW] Updating solved problem : 1874, 5430

--- sw/java/p01874/Main.java
+++ sw/java/p01874/Main.java
@@ -3,31 +3,38 @@ package p01874;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int i, k, max=0, stIdx = 0;
 		int N = Integer.parseInt(br.readLine());
-		int stack[] = new int[N]; StringBuilder sb = new StringBuilder();
+		List<Integer> inputList = new ArrayList<Integer>();
 		
-		while(N-->0) {
-			k = Integer.parseInt(br.readLine());
-			if(k>max) {
-				for(i=max; i<k; i++) {
-					stack[stIdx++] = i+1;
-					sb.append("+\n");
-				}
-			} else {
-				if(stack[stIdx-1]!=k) {
-					System.out.print("NO");
-					return;
-				}
-			}
-			stIdx--;
-			sb.append("-\n");
-			if(k>max) max = k;
+		for(int i=0; i<N; i++) {
+			inputList.add(Integer.parseInt(br.readLine()));
 		}
-		System.out.println(sb);
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 1; i <= N; i++) {
+			stack.push(i);
+			sb.append("+\n");
+
+			while (!stack.isEmpty() && !inputList.isEmpty() && stack.peek().equals(inputList.get(0))) {
+				stack.pop();
+				inputList.remove(0);
+				sb.append("-\n");
+			}
+		}
+		
+		if(!stack.isEmpty()) {
+			sb.setLength(0);
+			sb.append("NO\n");
+		}
+		System.out.print(sb.toString());
 	}
 }
--- sw/java/p05430/Main.java
+++ sw/java/p05430/Main.java
@@ -1,132 +1,75 @@
 package p05430;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.StringTokenizer;
-
-public class Main {
-
-	public static void main(String[] args) {
-		sc.init();
-
-		new Main().solve();
-	}
-
-	private void solve() {
-		int n = sc.nextInt();
-
+import java.util.LinkedList;
+class Solution{
+	public void solve() throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		
+		int n = Integer.parseInt(br.readLine());
+		
 		for (int i = 0; i < n; i++) {
-			String ops = sc.next();
-			int size = sc.nextInt();
-			int flag = 0;
+			String[] ops = br.readLine().split("");
+			int size = Integer.parseInt(br.readLine());
+			boolean flag = true;	// true : LEFT, false : RIGHT
 			boolean errFlag = false;
-			String tokens = sc.next();
-			tokens = tokens.replace("[", "");
-			tokens = tokens.replace("]", "");
-			StringBuffer res = new StringBuffer();
+			String[] tokens = br.readLine().replace("[", "").replace("]", "").split(",");
+			
 
-			String[] seta = tokens.split(",");
-			Deque<Integer> list = new ArrayDeque<Integer>();
+			Deque<Integer> list = new LinkedList<Integer>();
 			int[] data = new int[size];
 
 			for (int j = 0; j < size; j++) {
-				list.add(Integer.parseInt(seta[j]));
+				list.add(Integer.parseInt(tokens[j]));
 			}
-
-			for (int j = 0; j < ops.length(); j++) {
-				if (ops.charAt(j) == 'R') {
-					flag++;
-				} else {
-					if (list.size() > 0) {
-						if (flag % 2 == 1) {
-							list.removeLast();
-						} else {
-							list.removeFirst();
-						}
-					} else {
+			for(String oper : ops) {
+				if("R".equals(oper)) {
+					flag = !flag;
+				}else if("D".equals(oper)) {
+					if(list.isEmpty()) {
 						errFlag = true;
 						break;
+					} else {
+						if(flag) {
+							list.pollFirst();
+						} else {
+							list.pollLast();
+						}
 					}
 				}
 			}
-
+			StringBuffer res = new StringBuffer();
 			if(errFlag) {
-				System.out.println("error");
+				sb.append("error").append("\n");
 			} else {
 				res.append("[");
-				if(flag%2==0) {
+				int listSize = list.size();
+				if(flag) {
 					while(!list.isEmpty()) {
-						res.append(list.removeFirst()).append(",");
+						res.append(list.pollFirst()).append(",");
 					}
 				} else {
 					while(!list.isEmpty()) {
-						res.append(list.removeLast()).append(",");
+						res.append(list.pollLast()).append(",");
 					}
 				}
-				res = res.deleteCharAt(res.length()-1);
-				res.append("]");
-				System.out.println(res);
-			}
-			
-
-		}
-	}
-
-	static class sc {
-		private static BufferedReader br;
-		private static StringTokenizer st;
-
-		static void init() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-			st = new StringTokenizer("");
-		}
-
-		static String readLine() {
-			try {
-				return br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-
-		static String readLineReplace() {
-			try {
-				return br.readLine().replace("\\s+", "");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-
-		static String next() {
-			while (!st.hasMoreTokens()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
+				
+				if(listSize>0) {
+					res = res.deleteCharAt(res.length()-1);
 				}
+				res.append("]");
+				sb.append(res).append("\n");
+				
 			}
-			return st.nextToken();
 		}
-
-		static boolean hasNext() {
-			return st.hasMoreTokens();
-		}
-
-		static long nextLong() {
-			return Long.parseLong(next());
-		}
-
-		static int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		static double nextDouble() {
-			return Double.parseDouble(next());
-		}
+		System.out.print(sb);
 	}
+}
+public class Main {
 
+	public static void main(String[] args) throws Exception {
+		new Solution().solve();
+	}
 }
